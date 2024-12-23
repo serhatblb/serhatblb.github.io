d9345628c0880d002874d88c8d6b887ec059001b
Serhat Bülbül <serhatblb23@gmail.com>
2024-12-24 00:36:56 +0300

    new template

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleProjectDetails(projectId) {
-    const projectDetails = document.getElementById(projectId);
-    if (projectDetails.style.display === "block") {
-        projectDetails.style.display = "none";
-    } else {
-        projectDetails.style.display = "block";
-    }
-}
