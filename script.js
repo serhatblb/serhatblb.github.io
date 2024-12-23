2342ba14667f1b8673ff3cb2bdc6e914863585e7
Serhat Bülbül <serhatblb23@gmail.com>
2024-12-24 00:22:46 +0300

    duzeltme

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Welcome to my portfolio site!');
+function toggleProjectDetails(projectId) {
+    const projectDetails = document.getElementById(projectId);
+    if (projectDetails.style.display === "block") {
+        projectDetails.style.display = "none";
+    } else {
+        projectDetails.style.display = "block";
+    }
+}
